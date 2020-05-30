7ffcfe77eca4faaa4d1c9a6798f8634d21a9edd7
Pawan Akhil <pawanakhil.dasari@gmail.com>
2020-05-30 09:53:27 +0530

    first commit

---
+++ EventListener/EmailSubscriber.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace MauticPlugin\MauticAdvancedTemplatesBundle\EventListener;
+use Mautic\CampaignBundle\Entity\Lead;
+use Mautic\CoreBundle\EventListener\CommonSubscriber;
+use Mautic\EmailBundle\EmailEvents;
+use Mautic\EmailBundle\Event as Events;
+use Mautic\EmailBundle\Helper\PlainTextHelper;
+use Mautic\CoreBundle\Exception as MauticException;
+use MauticPlugin\MauticAdvancedTemplatesBundle\Helper\TemplateProcessor;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class EmailSubscriber.
+ */
+class EmailSubscriber extends CommonSubscriber
+{
+    /**
+     * @var TokenHelper $tokenHelper ;
+     */
+    protected $templateProcessor;
+
+
+    /**
+     * EmailSubscriber constructor.
+     *
+     * @param TokenHelper $tokenHelper
+     */
+    public function __construct(TemplateProcessor $templateProcessor)
+    {
+        $this->templateProcessor = $templateProcessor;
+    }
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            EmailEvents::EMAIL_ON_SEND => ['onEmailGenerate', 300],
+            EmailEvents::EMAIL_ON_DISPLAY => ['onEmailGenerate', 0],
+        ];
+    }
+
+    /**
+     * Search and replace tokens with content
+     *
+     * @param Events\EmailSendEvent $event
+     * @throws \Throwable
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Syntax
+     */
+    public function onEmailGenerate(Events\EmailSendEvent $event)
+    {
+        $this->logger->info('onEmailGenerate MauticAdvancedTemplatesBundle\EmailSubscriber');
+
+        if ($event->getEmail()) {
+            $subject = $event->getEmail()->getSubject();
+            $content = $event->getEmail()->getCustomHtml();
+        }else{
+            $subject = $event->getSubject();
+            $content = $event->getContent();
+        }
+
+        $originalTokens = $event->getTokens();
+        $tokens = [];
+        foreach($originalTokens as $k => $v) {
+            $tokens[preg_replace('/^{(.*)}$/', '${1}', $k)] = $v;
+        }
+
+        $subject = $this->templateProcessor->processTemplate($subject,  $event->getLead() , $tokens);
+        $event->setSubject($subject);
+
+        $content = $this->templateProcessor->processTemplate($content,  $event->getLead(), $tokens);
+        $event->setContent($content);
+
+
+        if ( empty( trim($event->getPlainText()) ) ) {
+            $event->setPlainText( (new PlainTextHelper($content))->getText() );
+         } else {
+            $plainText = $this->templateProcessor->processTemplate($event->getPlainText(), $event->getLead(), $tokens);
+            $event->setPlainText($plainText);
+        }
+    }
+}
---
+++ Helper/TemplateProcessor.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace MauticPlugin\MauticAdvancedTemplatesBundle\Helper;
+
+use MauticPlugin\MauticAdvancedTemplatesBundle\Feed\FeedFactory;
+use MauticPlugin\MauticCrmBundle\Integration\Salesforce\Object\Lead;
+use Psr\Log\LoggerInterface;
+
+class TemplateProcessor
+{
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var \Twig_Environment
+     */
+    private $twigEnv;
+    private $twigDynamicContentLoader;
+
+    private static $matchTwigBlockRegex = '/{%\s?TWIG_BLOCK\s?%}(.*?){%\s?END_TWIG_BLOCK\s?%}/ism';
+
+     /** @var  array */
+    private $tokens;
+
+    /** @var  array */
+    private $lead;
+
+    /**
+     * @var FeedFactory
+     */
+    private $feedFactory;
+
+    /**
+     * TemplateProcessor constructor.
+     *
+     * @param LoggerInterface            $logger
+     * @param Twig_Loader_DynamicContent $twigDynamicContentLoader
+     * @param FeedFactory                $feedFactory
+     */
+    public function __construct(LoggerInterface $logger, Twig_Loader_DynamicContent $twigDynamicContentLoader, FeedFactory $feedFactory)
+    {
+        $this->logger = $logger;
+        $this->twigDynamicContentLoader = $twigDynamicContentLoader;
+        $logger->debug('TemplateProcessor: created $twigDynamicContentLoader');
+        $this->twigEnv = new \Twig_Environment(new \Twig_Loader_Chain([
+            $twigDynamicContentLoader, new \Twig_Loader_Array([])
+        ]));
+        $this->configureTwig($this->twigEnv);
+        $this->feedFactory = $feedFactory;
+    }
+
+
+    /**
+     * @param string $content
+     * @param array $lead
+     * @return string
+     * @throws \Throwable
+     */
+    public function processTemplate($content, $lead, $tokens)
+    {
+        $this->logger->debug('TemplateProcessor: Processing template');
+        $this->logger->debug('LEAD: ' . var_export($lead, true));
+        $this->logger->debug('TOKENS: ' . var_export($tokens, true));
+        $content = preg_replace_callback_array([
+            TemplateProcessor::$matchTwigBlockRegex => $this->processTwigBlock($lead , $tokens)
+            
+        ], $content);
+        $this->logger->debug('TemplateProcessor: Template processed');
+        return $content;
+    }
+
+    protected function configureTwig(\Twig_Environment $twig)
+    {
+        // You might want to register some custom TWIG tags or functions here
+
+        // TWIG filter json_decode
+        $twig->addFilter(new \Twig_SimpleFilter('json_decode', function ($string) {
+            return json_decode($string, true);
+        }));
+
+        $twig->addFilter(new \Twig_SimpleFilter('rss', function () {
+            return $this->feedFactory->getItems($this->lead['id'], func_get_args());
+        }));
+    }
+
+    private function processTwigBlock($lead, $tokens)
+    {
+        $this->lead = $lead;
+        $this->tokens = $tokens ;
+        return function ($matches) use ($lead , $tokens) {
+            $templateSource = $matches[1];
+            $this->logger->debug('BLOCK SOURCE: ' . var_export($templateSource, true));
+            $template = $this->twigEnv->createTemplate($templateSource);
+            $renderedTemplate = $template->render(array_merge($tokens , [
+                'lead' => $lead
+            ]));
+            $this->logger->debug('RENDERED BLOCK: ' . var_export($renderedTemplate, true));
+            return $renderedTemplate;
+        };
+    }
+}
